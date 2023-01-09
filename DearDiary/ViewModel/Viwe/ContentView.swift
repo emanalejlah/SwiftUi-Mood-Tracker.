6f2c932cbc1361c827ff9ffdf01ebc49fa4e2d41
Eman <eaalejlah22@twq.idserve.net>
2023-01-09 22:54:49 +0300

    m

---
+++ DearDiary/ViewModel/Viwe/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  DearDiary2
+//
+//  Created by eman alejilah on 16/06/1444 AH.
+//
+
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+
+
+    var body: some View {
+      
+            Home()
+
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
